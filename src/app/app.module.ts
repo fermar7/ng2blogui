9f4d21fea504cd7d2fee4d87273cd0f151fcf7fd
pfeiffer <marius.pfeiffer@foerch.de>
2017-01-24 14:47:38 +0100

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'
+import { routing } from './app.routing'
+
+import { ModuleWithProviders } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+
+import { CommonModule } from './common/common.module'
+import { SidebarModule } from './sidebar/sidebar.module'
+import { EntriesModule } from './entries/entries.module'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CommonModule,
+    SidebarModule,
+    RouterModule,
+    EntriesModule,
+    routing
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
